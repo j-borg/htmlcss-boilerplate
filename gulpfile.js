bb1155e22bed7ce87883525ba814312bbed98d71
Jille <jilleborg@hotmail.com>
2019-08-19 22:05:05 +0200

    feat(): init boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', () => {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+  })
+});
+
+gulp.task('sass', () => (
+  gulp.src('src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./src/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+));
+
+gulp.task('html', () =>(
+  gulp.watch('src/*.html', browserSync.reload)
+));
+
+gulp.task('sass:watch', ['browserSync', 'sass', 'html'], () => (
+  gulp.watch('src/scss/**/*.scss', ['sass'])
+));
+
+gulp.task('default', ['sass', 'sass:watch']);
+gulp.task('build', ['sass']);
